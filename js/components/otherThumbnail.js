3171457443b5b58ea58fb74ea92ac4a8e37cecf2
Eduardo Schlichting <eduardoschlichting@Eduardos-MacBook-Pro.local>
2021-10-30 10:23:23 -0400

    Adding Midterm Specs

--- js/components/otherThumbnail.js
+++ js/components/otherThumbnail.js
@@ -2,12 +2,13 @@ export default {
     name: "TheOtherThumbnailComponent",
 
     template: `
-            <div class="project-box__other">
-              <div class="project-box__content">
-                <h3 class="other-project-h  eading">
-                  Project 1
-                </h3>
-              </div>
-            </div>
+      <div class="other">
+
+        <div class="project-box__content">
+          <h3 class="project-heading">Project 1</h3>
+          <h4>year</h4>
+        </div>
+
+      </div>
     `
 }
