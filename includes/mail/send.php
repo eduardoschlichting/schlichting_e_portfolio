d1b4df5507df358f7c5d6d5e0f617b06f4237285
Eduardo Schlichting <eduardoschlichting@Eduardos-MacBook-Pro.local>
2021-12-02 13:40:57 -0500

    test

--- includes/mail/send.php
+++ includes/mail/send.php
@@ -1,95 +1,105 @@
 <?php
 
-// DEBUG ONLY, remove after
-error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', '1');
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json; charset=UTF-8');
-
-// variables to be filled
 $results = [];
 $visitor_name = '';
 $visitor_email = '';
-
 $visitor_message = '';
-$empty_name = '';
+$name_error = '';
 $email_error = '';
-$message_error = '';
-
-// $canMail = TRUE;
 
 
-// REQUIRED FIELDS
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['firstname'])) {
-        $empty_name = "Please enter your First Name.";
-        // $canMail = FALSE;
+        $name_error = "First Name is Required!";
     } else {
         $visitor_name = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
     }
 
     if (empty($_POST['lastname'])) {
-        $empty_name = "Please enter your Last Name.";
+        $name_error = "Last Name is Required!";
     } else {
-        $visitor_name .= ' ' . filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
+        $visitor_name .= ' '.filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
     }
 
     if (empty($_POST['email'])) {
-        $email_error = "Please enter your e-mail.";
+        $email_error = "Email is Required!";
     } else {
         $visitor_email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
     }
 
-    if (empty($_POST['message'])) {
-        $message_error = "Please tell me a bit about your project.";
-    } else {
+    if (isset($_POST['message'])) {
         $visitor_message = filter_var(htmlspecialchars($_POST['message']), FILTER_SANITIZE_STRING);
     }
 }
 
+
 $results['name'] = $visitor_name;
 $results['email'] = $visitor_email;
 $results['message'] = $visitor_message;
-$results['empty_name'] = $empty_name;
-$results['email_error'] = $email_error;
-$results['message_error'] = $message_error;
+$results['name_error'] = $name_error;
+$results['email-error'] = $email_error;
 
-// 2. Prepare the email [Print ot the label and put on the pachage / Prepare the package in certain format]
+
+
+// prepare the email
 $email_subject = 'Inquiry from Portfolio Site';
-$email_recipient = 'schlichting.eduardo@gmail.com'; //your email, or AKA, 'To' email
-$email_title = 'Inquiry from Portfolio Site';
-$to = 'eduschlichting@eduardoschlichting.com';
-
-$body = "";
-$body .= "From: ".$visitor_name. "\r\n";
-$body .= "Email: ".$visitor_email. "\r\n";
-$body .= "Message: ".$visitor_message. "\r\n";
-
+$email_recipient = 'schlichting.eduardo@gmail.com';
+$email_message = sprintf('Name: %s, Email: %s, Message: %s', $visitor_name, $visitor_email, $visitor_message);
 $email_headers = array(
 
-    'From'=>$user_email
+    'From' =>$visitor_email
 );
+// echo $email_recipient;
 
-// 3. Send out the email
-// if the fields are not empty and the form was requested:
-// if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
-//     $email_result = mail($to, $email_title, $email_subject, $body, $email_headers);
-//     if($email_result){
-//         $results['message'] = sprintf('Thank you for contacting me, %s. I will get back to you as soon as I can!', $user_name);
-        
+// // switch emails
+// $subject = $_POST['subject'];
+// if ($subject == 'Design') { //if career was selected
+//     $to = $email_recipient;
+// } else { //other options
+//     $to = 'shirinhk11@gmail.com';
+// }
+
+//reCAPTCHA
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['recaptcha_response'])) {
+//     // Build POST request:
+//     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+//     $recaptcha_secret = '6Ldc2lQdAAAAAELmW67qcyMxZcXhwR_IfexDrLI9';
+//     $recaptcha_response = $_POST['recaptcha_response'];
+
+//     // Make and decode POST request:
+//     $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+//     $recaptcha = json_decode($recaptcha);
+
+//     // Take action based on the score returned:
+//     if ($recaptcha->score >= 0.5) {
+//         // Verified - send email
 //     } else {
-//         $results['message'] = sprintf('I am sorry, but the email did not go through.');
+//         // Not verified - show form error
 //     }
 // }
 
-$email_result = mail($to, $email_title, $email_subject, $body, $email_headers);
-    if($email_result){
-        $results['message'] = sprintf('Thank you for contacting me, %s. I will get back to you as soon as I can!', $user_name);
-        echo json_encode($message => $results['message']);
-    } else {
-        $results['message'] = sprintf('I am sorry, but the email did not go through.');
-        echo "email failed";
-    }
+
+//send out the email
+$email_result = mail($email_recipient, $email_subject, $email_message, $email_headers);
+if ($email_result) {
+    $results['message'] = sprintf('Thank you for contacting me, %s. I will reply within 24 hours.', $visitor_name);
+} else {
+    $result['message'] = sprintf('Sorry! This email did not go through. Please try again.');
+}
+
+
+echo json_encode($results);
+
+// $email_result = mail($to, $email_title, $email_subject, $body, $email_headers);
+//     if($email_result){
+//         $results['message'] = sprintf('Thank you for contacting me, %s. I will get back to you as soon as I can!', $user_name);
+//     } else {
+//         $results['message'] = sprintf('I am sorry, but the email did not go through.');
+//         echo "email failed";
+//     }
 
 
 
