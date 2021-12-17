84ba225123becb590da347ddbbab9ac18c21c9d1
Eduardo Schlichting <eduardoschlichting@Eduardos-MacBook-Pro.local>
2021-12-17 09:08:21 -0500

    updating layout december 2021

--- js/components/lightbox.js
+++ js/components/lightbox.js
@@ -24,21 +24,19 @@ export default {
             <a :href='item.Behance' target="_blank" v-if="item.Behance"><h3>See the Project on Behance</h3></a>
 
             <p class="desc_1" v-if="item.Desc1">{{item.Desc1}}</p>
-            <img class="img_lightbox" v-if="item.Image1" :src='"dist/" + item.Image1' alt="item.Image1">
-
             <p class="desc_2" v-if="item.Desc2">{{item.Desc2}}</p>
-            <img class="img_lightbox" v-if="item.Image2" :src='"dist/" + item.Image2' alt="item.Image2">
-
             <p class="desc_3" v-if="item.Desc3">{{item.Desc3}}</p>
-            <img class="img_lightbox" v-if="item.Image3" :src='"dist/" + item.Image3' alt="item.Image3">
-
             <p class="desc_4" v-if="item.Desc4">{{item.Desc4}}</p>
 
+            <img class="img_lightbox" v-if="item.Image1" :src='"dist/" + item.Image1' alt="item.Image1">   
+            <img class="img_lightbox" v-if="item.Image2" :src='"dist/" + item.Image2' alt="item.Image2">
+            <img class="img_lightbox" v-if="item.Image3" :src='"dist/" + item.Image3' alt="item.Image3">
             <img class="img_lightbox" v-if="item.Image4" :src='"dist/" + item.Image4' alt="item.Image4">
             <img class="img_lightbox" v-if="item.Image5" :src='"dist/" + item.Image5' alt="item.Image5">
+
             <div class="video_lightbox">
             <video class="img_lightbox" id="video_lightbox" v-if="item.video" :src='"dist/" + item.video' controls alt="item.video" key="video"></video>
-        </div>
+            </div>
         
         <section class="contact-cta contact-lightbox">
           <div class="contact-wrapper">
